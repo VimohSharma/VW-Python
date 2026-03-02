2eeecc1a566a1e54bfd173cbc77a296d4c7a9e2d
sharmavimoh <91205291+VimohSharma@users.noreply.github.com>
2026-03-02 10:19:40 +0530

    Update Assignment1.py
    made some changes

--- Vimoh Sharma/Assignments/Day16/Assignment1.py
+++ Vimoh Sharma/Assignments/Day16/Assignment1.py
@@ -5,7 +5,8 @@ app=Flask(__name__)
 
 @app.route("/add/<int:num1>/<int:num2>")
 def addition(num1,num2):
-    return f"{num1} + {num2} = {str(num1+num2)}"
+    return f"{num1} + {num2} = {num1+num2}"
 
 if __name__ == "__main__":
+
     app.run(debug=True)
