0551f6a8adc9af027ae37168e5766bd01087d330
sharmavimoh <91205291+VimohSharma@users.noreply.github.com>
2026-03-02 10:17:52 +0530

    Day 16 Assignements pt.1

---
+++ Vimoh Sharma/Assignments/Day16/Assignment1.py
@@ -0,0 +1,11 @@
+
+from flask import Flask, render_template, jsonify
+
+app=Flask(__name__)
+
+@app.route("/add/<int:num1>/<int:num2>")
+def addition(num1,num2):
+    return f"{num1} + {num2} = {str(num1+num2)}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
